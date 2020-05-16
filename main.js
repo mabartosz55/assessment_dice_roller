0dcfbd3a779e2f6a5fab24c71f0000c6ba717bc7
michael bartosz <michaelbartosz@michaels-iMac.local>
2020-05-16 11:21:17 -0400

    having issues

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let inputNumber = document.querySelector("#numberRolls")
+let rollButton = document.querySelector("#submit")
+
+
+rollButton.addEventListener("click", function() {
+
+    console.log(rollButton)
+
+    
+
+
+
+})
+
