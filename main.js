d8f96cf35f1f397fbe22adc219c207e19dd08081
michael bartosz <michaelbartosz@michaels-iMac.local>
2020-05-17 14:28:52 -0400

    lost on this

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 
 let inputNumber = document.querySelector("#numberRolls")
 let rollButton = document.querySelector("#submit")
+let rollTotal = document.querySelector("#sumOfRolls")
+
+let counter = 0
 
 
-rollButton.addEventListener("click", function() {
+rollButton.addEventListener("click", function () {
+    inputNumber.value = inputNumber.innerHTML.value
+    function inputNumber(number) {
+        return Math.floor(Math.random() * (number + 1));
+    }
 
-    console.log(rollButton)
 
-    
 
+    rollTotal.innerHTML = Number(rollTotal.innerHTML) + 1;
+
+    //while (rollTotal = rollTotal.innerHTML) [counter];
+
+    //if (rollTotal.innerHTML >= 0) + 1 
+    // randomNumberVar.innerHTML = Number(rollTotal.innerHTML) + 1;
 
 
 })
 
+
+//let randomNumberVar = Math.random()
+//console.log(randomNumberVar)
+//if (randomNumberVar > 0.5 * 10) {
+//    inputNumber.innerHTML = Number(rollTotal.innerHTML) + 1;
+
+
+console.log(rollButton)
+console.log(rollTotal)
