f48a261a97ce84281ed18740f63396f5bde7cab1
michael bartosz <michaelbartosz@michaels-iMac.local>
2020-05-19 20:04:00 -0400

    almost there

--- main.js
+++ main.js
@@ -1,35 +1,47 @@
-
+let dieRolls = [];
 let inputNumber = document.querySelector("#numberRolls")
 let rollButton = document.querySelector("#submit")
-let rollTotal = document.querySelector("#sumOfRolls")
+let rollSum = document.querySelector("#sumOfRolls")
+let countRolls = document.querySelector("#roll")
+let showAllRolls = document.querySelector("#showAllRolls")
+let resetButton = document.querySelector("#reset")
+
 
 let counter = 0
 
-
 rollButton.addEventListener("click", function () {
-    inputNumber.value = inputNumber.innerHTML.value
-    function inputNumber(number) {
-        return Math.floor(Math.random() * (number + 1));
+    let userinput = inputNumber.value
+    while (counter < userinput) {
+        counter += 1
+        dieRolls.push(Math.floor(Math.random() * 6 + 1))
+
+        //console.log(dieRolls)
     }
 
+    let rollSum2 = 0
+    for (let counter2 = 0; counter2 < dieRolls.length; counter2 += 1) {
+        rollSum2 += dieRolls[counter2]
 
+        //console.log(dieRolls)
+    }
+    countRolls.innerHTML = rollSum2
 
-    rollTotal.innerHTML = Number(rollTotal.innerHTML) + 1;
+    showAllRolls.addEventListener("click", function () {
+        for (counter2 = 0; counter2 < dieRolls.length; counter2++)
+        //let numberRolled = dieRolls[counter2]
+        //const newDiceString = '<li class="dice">' + numberRolled + "</li>";
+        //allRollsElement.innerHTML += newDiceString;
 
-    //while (rollTotal = rollTotal.innerHTML) [counter];
+        console.log(newDiceString)
 
-    //if (rollTotal.innerHTML >= 0) + 1 
-    // randomNumberVar.innerHTML = Number(rollTotal.innerHTML) + 1;
+    })
+
+    //resetButton.addEventListener("click", function () {
+    //resetNumber.innerHTML = Number(resetNumber.innerHTML) + 1;
 
 
 })
 
 
-//let randomNumberVar = Math.random()
-//console.log(randomNumberVar)
-//if (randomNumberVar > 0.5 * 10) {
-//    inputNumber.innerHTML = Number(rollTotal.innerHTML) + 1;
 
 
-console.log(rollButton)
-console.log(rollTotal)
